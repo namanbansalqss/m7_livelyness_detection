4ef49aba4c0743159830541a108d10c0f009bdf9
namanbansalqss <110884069+namanbansalqss@users.noreply.github.com>
2024-02-17 19:37:08 +0530

    Update ml_kit_extesion.dart

--- lib/src/core/extensions/ml_kit_extesion.dart
+++ lib/src/core/extensions/ml_kit_extesion.dart
@@ -2,39 +2,47 @@ import 'package:m7_livelyness_detection/index.dart';
 
 extension MLKitUtils on AnalysisImage {
   InputImage toInputImage() {
-    final planeData =
-        when(nv21: (img) => img.planes, bgra8888: (img) => img.planes)?.map(
-      (plane) {
-        return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: height,
-          width: width,
-        );
-      },
-    ).toList();
+    // final planeData =
+    //     when(nv21: (img) => img.planes, bgra8888: (img) => img.planes)?.map(
+    //   (plane) {
+    //     return InputImageMetadata(
+    //       bytesPerRow: plane.bytesPerRow,
+    //       size: Size(
+    //         width.toDouble(),
+    //         height.toDouble(),
+    //       ),
+    //       rotation: inputImageRotation,
+    //       format: InputImageFormat.nv21,
+    //       // height: height,
+    //       // width: width,
+    //     );
+    //   },
+    // ).toList();
 
     return when(nv21: (image) {
       return InputImage.fromBytes(
         bytes: image.bytes,
-        inputImageData: InputImageData(
-          imageRotation: inputImageRotation,
-          inputImageFormat: InputImageFormat.nv21,
-          planeData: planeData,
+        metadata: InputImageMetadata(
+          bytesPerRow: image.planes.first.bytesPerRow,
           size: image.size,
+          rotation: inputImageRotation,
+          format: InputImageFormat.nv21,
+          // height: height,
+          // width: width,
         ),
       );
     }, bgra8888: (image) {
-      final inputImageData = InputImageData(
+      final inputImageData = InputImageMetadata(
         size: size,
         // FIXME: seems to be ignored on iOS...
-        imageRotation: inputImageRotation,
-        inputImageFormat: inputImageFormat,
-        planeData: planeData,
+        rotation: inputImageRotation,
+        format: inputImageFormat,
+        bytesPerRow: image.planes.first.bytesPerRow,
       );
 
       return InputImage.fromBytes(
         bytes: image.bytes,
-        inputImageData: inputImageData,
+        metadata: inputImageData,
       );
     })!;
   }
